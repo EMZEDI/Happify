41b26934c907bbcc47790b51c0440a74d977f488
Tristan Stevens <tvut@outlook.com>
2022-11-19 11:25:40 -0500

    added router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
 import App from './App.vue'
+
 import './assets/main.css'
 
-createApp(App).mount('#app')
+import PlayerPage from './components/PlayerPage.vue'
+import PlaylistSetup from './components/PlaylistSetup.vue'
+
+
+const routes = [
+    { path: '/', component: PlaylistSetup },
+    { path: '/dashboard', component: PlayerPage },
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+
+createApp(App).use(router).mount('#app')
